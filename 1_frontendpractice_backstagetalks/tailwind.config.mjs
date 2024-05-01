e3ca915e6a457105c5cd8330c794c07b953d3f08
Andrew DGalga <ex.spake@gmail.com>
2024-05-01 15:40:01 -0500

    color base. Contrast issues on Blue and Red pages, will need to modify font colors. Frontendpractice doesn't have the most complete color list, treating as design and not counting against hour.

--- 1_frontendpractice_backstagetalks/tailwind.config.mjs
+++ 1_frontendpractice_backstagetalks/tailwind.config.mjs
@@ -2,6 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors : {
+			white: '#fff',
+			pink:{ 
+				100: '#e568ac',
+				200: '#ff608c'
+			},
+			black: {
+				100:'#251d20',
+
+				300: '#000'
+			},
+			cyan: '#00c1b5',
+			orange: '#ff6519',
+			yellow: '#ffbe00',
+			blue: '#1d3fbb',
+			red: '#E30512'
+		},
 		extend: {},
 	},
 	plugins: [],
