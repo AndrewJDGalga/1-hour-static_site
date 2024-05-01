1a3edb292009f3473833c545f8705b68fc3b9042
Andrew DGalga <ex.spake@gmail.com>
2024-05-01 16:08:51 -0500

    added and tested font working the easy way - in html head.

--- 1_frontendpractice_backstagetalks/tailwind.config.mjs
+++ 1_frontendpractice_backstagetalks/tailwind.config.mjs
@@ -1,7 +1,12 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily : {
+			'sans': ['Roboto', defaultTheme.fontFamily.sans]
+		},
 		colors : {
 			white: '#fff',
 			pink:{ 
@@ -10,7 +15,6 @@ export default {
 			},
 			black: {
 				100:'#251d20',
-
 				300: '#000'
 			},
 			cyan: '#00c1b5',
