b7301f2bfdf6e0f811e50071bfe5c7020b777dd6
Andrew DGalga <ex.spake@gmail.com>
2024-05-08 00:25:15 -0500

    colors and font, looks like futura-pt is default but imported anyway.

--- 2_frontendpractice_ableton/tailwind.config.mjs
+++ 2_frontendpractice_ableton/tailwind.config.mjs
@@ -1,7 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily:{
+			'sans': ['futura-pt', defaultTheme.fontFamily.sans]
+		},
+		colors:{
+			white:'white',
+			black: 'black',
+			orange: '#FF764D',
+			yellow: '#FBFFA7',
+			'blue-100': '#B1C5FF',
+			'blue-200' : 'blue',
+			green : '#B6FFC0',
+			purple : '#D5B3FF'
+		},
 		extend: {},
 	},
 	plugins: [],
